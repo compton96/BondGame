f1775d3281b854651ffb6b5cda7682c914bef654
riko <ewidodo1998@gmail.com>
2020-12-30 15:37:39 -0800

    Replaced all the placeholder classes >:(

---
+++ Assets/_Bond/Scripts/Creatures/CreatureAI.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureAI : MonoBehaviour
+{
+    public BTNode behaviorTree;
+    private CreatureAIContext context;
+    public List<Personality> personalities = new List<Personality>();
+    public Personality DefaultPersonality;
+
+    public bool Evaluate = false;
+
+    private void Start()
+    {
+        context = GetComponent<CreatureAIContext>();
+        //BuildBT();
+    }
+
+    private void Awake() {
+        context = GetComponent<CreatureAIContext>();
+    }
+
+    private void Update() {
+        if(Evaluate){
+            behaviorTree.Evaluate();
+            context.animator.Move(context.agent.velocity);
+        }
+    }
+
+
+    private void FixedUpdate() {
+        //behaviorTree.Evaluate();
+    }
+
+
+    //build the behavior tree for the creature
+    public void BuildBT() 
+    {
+        personalities = context.CD.personalities;
+        List<BTNode> RootList = new List<BTNode>();
+
+        #region BONDED FOLLOW PLAYER
+            BTSelector FollowPlayer = null;
+            foreach( Personality p in personalities){
+                if(p.FollowPlayerTree != null){
+                    FollowPlayer = p.FollowPlayerTree.BuildSelectorSubtree(context);
+                }  
+            }
+            if(FollowPlayer == null){
+                FollowPlayer = DefaultPersonality.FollowPlayerTree.BuildSelectorSubtree(context);
+            }
+        #endregion
+
+        #region CREATURE ABILITIES
+            BTSequence Ability = null;
+            foreach( Personality p in personalities){
+                if(p.AbilityTree != null){
+                    Ability = p.AbilityTree.BuildSequenceSubtree(context);
+                }  
+            }
+            if(Ability == null){
+                Ability = DefaultPersonality.AbilityTree.BuildSequenceSubtree(context);
+            }
+        #endregion
+
+
+        #region WILD PLAYER
+            BTSequence wildNoticed = null;
+            foreach( Personality p in personalities){
+                if(p.WildNoticed != null){
+                    wildNoticed = p.WildNoticed.BuildSequenceSubtree(context);
+                }  
+            }
+            if(wildNoticed == null){
+                wildNoticed = DefaultPersonality.WildNoticed.BuildSequenceSubtree(context);
+            }
+        #endregion
+
+        //add this section later
+        #region WILD NO PLAYER
+            BTSelector wildAlone = null;
+            foreach( Personality p in personalities){
+                if(p.WildAlone != null){
+                    wildAlone = p.WildAlone.BuildSelectorSubtree(context);
+                }  
+            }
+            if(wildAlone == null){
+                wildAlone = DefaultPersonality.WildAlone.BuildSelectorSubtree(context);
+            }
+        #endregion
+
+        #region IS CREATURE WILD
+            #region creature is wild
+                List<BTNode> CreatureIsWildList = new List<BTNode>();
+                CreatureIsWildList.Add(wildNoticed);
+                //be sure to add the no player section later
+                CreatureIsWildList.Add(wildAlone); //placeholder for wild w/ no player section
+
+                BTSelector creatureIsWildSelector = new BTSelector("Creature Is Wild", CreatureIsWildList);
+            #endregion
+
+            #region is creature wild
+                List<BTNode> IsCreatureWildList = new List<BTNode>();
+                BTCheckIsWild isWild = new BTCheckIsWild("Is Wild?", context);
+                IsCreatureWildList.Add(isWild);
+                IsCreatureWildList.Add(creatureIsWildSelector);
+                BTSequence isCreatureWildSequence = new BTSequence("Is Creature Wild?", IsCreatureWildList);
+            #endregion
+        #endregion
+
+        #region creature isnt wild selector
+            List<BTNode> CreatureIsntWildSelectorList = new List<BTNode>();
+            CreatureIsntWildSelectorList.Add(Ability);
+            CreatureIsntWildSelectorList.Add(FollowPlayer);
+
+            BTSelector CreatureIsntWildSelector = new BTSelector("Creature isnt Wild Selector", CreatureIsntWildSelectorList);
+        #endregion
+
+        #region ROOT
+            RootList.Add(isCreatureWildSequence);
+            RootList.Add(CreatureIsntWildSelector);
+
+            BTSelector _root = new BTSelector("Root", RootList);
+        #endregion
+        behaviorTree = _root;
+        Evaluate = true;
+    }
+
+}
---
+++ Assets/_Bond/Scripts/Creatures/CreatureAIContext.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+
+[System.Serializable]
+public class CreatureAIContext : MonoBehaviour
+{
+    #region Behavior Tree Context
+
+    [Header("Objects")]
+    public GameObject player;
+    public GameObject targetEnemy;
+    public GameObject cleverItem; //interesting items, only for clever creatures
+    public GameObject foundFood; //Used in Hearty personality to find food
+    public Transform creatureTransform;
+    public Rigidbody rb;
+    public ActiveCreatureData CD;
+    public creatureData creatureTypeData;
+    public NavMeshAgent agent;
+    public GameObject backFollowPoint;
+    public GameObject followPoint;
+    public GameObject projectileSpawner;
+    public CreatureAnimator animator;
+    
+
+    
+    [Header("Bools")]
+    public bool isWild;
+    public bool isInPlayerRadius;
+    public bool isInPlayerTrail;
+    public bool isNoticed;
+    public bool isAbilityTriggered;
+    public bool wanderIdling = false;
+    public bool cleverIgnoreItems = false;
+
+    [Header("Misc.Numbers")]
+    public float playerSpeedToScare;
+    public int lastTriggeredAbility;
+    public float enemyDetectRange;
+    public float itemDetectRange; //range for detecting interesting items, only for clever creatures
+    public float wanderRadius; //how far from starting location the creature can wander
+    public float wanderIdleDuration;
+    public float wanderIdleTimer;
+    public Vector3 wanderDestination;
+    public Vector3 wildStartingLocation;
+    public float stealDuration;
+    public float stealTimer;
+
+
+    private int debugNumber;
+    private CreatureDebugText debugText;
+    #endregion
+
+    private void Awake()
+    {
+        creatureTransform = transform;
+        wildStartingLocation = creatureTransform.position;
+        animator = GetComponent<CreatureAnimator>();
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        followPoint = GameObject.FindGameObjectWithTag("FrontFollowPoint");
+        backFollowPoint = GameObject.FindGameObjectWithTag("BackFollowPoint");
+        GameObject temp = GameObject.FindGameObjectWithTag("CreatureDebugText");
+        debugText = temp.GetComponent<CreatureDebugText>();
+        debugText.creaturesDebug.Add("");
+        debugNumber = debugText.creaturesDebug.Count - 1;
+        if(isWild){
+            lastTriggeredAbility = 0;
+        }
+
+        resetStealTimer();
+    }
+
+    public void GetActiveCreatureData(){
+        CD = GetComponent<ActiveCreatureData>();
+    }
+
+    private void FixedUpdate() {
+
+    }
+
+
+    public void doMovement(float moveSpeed){
+        rb.velocity = (creatureTransform.transform.rotation * Vector3.forward * moveSpeed);
+    }
+
+    public void doRotation(float rotationSpeed, Quaternion desiredLook) {
+        creatureTransform.rotation = Quaternion.Slerp(creatureTransform.rotation, desiredLook, Time.deltaTime * rotationSpeed); //10 is rotation speed - might want to change later
+    }
+
+    public void doLookAt(Vector3 position){
+        creatureTransform.transform.LookAt(position, Vector3.up);
+        rb.velocity = (creatureTransform.transform.rotation * Vector3.forward * CD.moveSpeed);
+    }
+
+
+    public void resetStealTimer() {
+        stealTimer = 0;
+        stealDuration = Random.Range(2f, 3f);
+    }
+    
+    public void updateDebugText(string name) {
+        debugText.creaturesDebug[debugNumber] = gameObject.name + " : " + name + "\n";
+    }
+}
