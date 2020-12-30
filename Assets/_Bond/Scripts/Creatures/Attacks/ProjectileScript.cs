98efef443f306cad14b445f175b0a8e1107b1afb
Colin Watterson <ctwatterson123@gmail.com>
2020-12-30 15:29:02 -0800

    triggers, attack base and misc

---
+++ Assets/_Bond/Scripts/Creatures/Attacks/ProjectileScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    GameObject target;
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    float speed = 10;
+    public float damage = 50;
+    public bool isHoming = false;
+
+    void Start()
+    {
+        
+    }
+
+    private void Awake() 
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            if(isHoming) 
+            {
+                transform.LookAt(target.transform.position);
+            }
+            
+            rigidBody.velocity = (transform.rotation*Vector3.forward*speed);
+        }
+    }
+
+    public void setTarget(GameObject _target, float _speed, float _damage, bool _isHoming)
+    {
+        target = _target;
+        transform.LookAt(target.transform.position);
+        speed = _speed;
+        damage = _damage;
+        isHoming = _isHoming;
+    }
+}
