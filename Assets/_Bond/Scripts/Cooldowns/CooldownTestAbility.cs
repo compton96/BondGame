57d6d3c280b8db09512535e0c17057caf568c85c
Eugene Shin <shin.eugene97@gmail.com>
2021-01-18 12:02:26 -0800

    Cooldown System is Implemented
    Modular and should be able to be slapped onto anything we want a cooldown for.

---
+++ Assets/_Bond/Scripts/Cooldowns/CooldownTestAbility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooldownTestAbility : MonoBehaviour , HasCooldown
+{
+    [SerializeField] private CooldownSystem cooldownSystem = null;
+
+    [SerializeField] private int id = 1;
+    [SerializeField] private float cooldownDuration = 5f;
+
+    public int Id=>id;
+    public float CooldownDuration => cooldownDuration;
+    private void Update()
+    {
+        // if (!Keyboard.current.spaceKey.wasPressedThisFrame)
+        // {
+        //     return;
+        // }
+    }
+}
