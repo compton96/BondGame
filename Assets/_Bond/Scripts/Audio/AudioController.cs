9232bb2973b03026c835760fca37d1a8bf5af0f2
riko <ewidodo1998@gmail.com>
2021-01-20 23:00:28 -0800

    Initial setup of FMOD stuff

---
+++ Assets/_Bond/Scripts/Audio/AudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public FMODUnity.StudioEventEmitter masterMusicEvent;
+    public FMODUnity.StudioEventEmitter ambientNoiseEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
