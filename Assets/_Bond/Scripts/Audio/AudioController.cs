ffbc74910ceb3987c132a74e712a9577707d7a1c
riko <ewidodo1998@gmail.com>
2021-01-21 17:29:21 -0800

    Got music working, will probably tweak a bit more later on

--- Assets/_Bond/Scripts/Audio/AudioController.cs
+++ Assets/_Bond/Scripts/Audio/AudioController.cs
@@ -7,6 +7,11 @@ public class AudioController : MonoBehaviour
     public FMODUnity.StudioEventEmitter masterMusicEvent;
     public FMODUnity.StudioEventEmitter ambientNoiseEvent;
 
+    public Transform playerTransform;
+    private bool inCombat = false;
+    public float enemyDetectRange;
+    private int layerMask = 1 << 8; //to target only layer 8 - enemies
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,39 @@ public class AudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //check if player is near enemies
+        Collider[] hitColliders = Physics.OverlapSphere(playerTransform.position, enemyDetectRange, layerMask);
+
+        //enemy found, prep combat music
+        if (hitColliders.Length > 0)
+        {
+            //if not yet in combat, transition to combat music
+            if (!inCombat)
+            {
+                masterMusicEvent.SetParameter("Combat State", 1);
+                masterMusicEvent.SetParameter("Song 2 - State", 0);
+                inCombat = true;
+            }
+            else 
+            {
+                //already in combat music
+                Collider[] nearHitCollider = Physics.OverlapSphere(playerTransform.position, enemyDetectRange * 3 / 4, layerMask);
+                if (nearHitCollider.Length > 0)
+                {
+                    masterMusicEvent.SetParameter("Song 2 - State", 1);
+                }
+            }
+        } 
+        //no enemies found
+        else
+        {
+            //if in combat, transition out of combat music
+            if (inCombat)
+            {
+                masterMusicEvent.SetParameter("Combat State", 0);
+                masterMusicEvent.SetParameter("Song 2 - State", 2);
+                inCombat = false;
+            }
+        }
     }
 }
