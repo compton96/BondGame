4f85c4f2a90a630c2851fa40090a76504a7a8af4
Colin Watterson <ctwatterson123@gmail.com>
2021-01-22 16:48:05 -0800

    Stat System coded not tested

---
+++ Assets/_Bond/Scripts/StatModifier/StatManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatManager : MonoBehaviour
+{
+    public BaseStats baseStats;
+    protected Dictionary<ModiferType, Stat> stats = new Dictionary<ModiferType, Stat>();
+    protected Dictionary<Buff, float> buffs = new Dictionary<Buff, float>();
+
+    private void Awake() 
+    {
+    
+
+    }
+
+    private void initStats() {
+        foreach(baseStat stat in baseStats.stats)
+        {
+            stats.Add(stat.modiferType, new Stat(stat.modiferType, stat.baseValue));
+        }
+    }
+
+    private void addModifer(Modifier _modifier)
+    {
+        if(stats.ContainsKey(_modifier.modiferType))
+        {
+            stats[_modifier.modiferType].addModifer(_modifier);
+        }
+    }
+
+    private void removeModifier(Modifier _modifier)
+    {
+        if(stats.ContainsKey(_modifier.modiferType))
+        {
+            stats[_modifier.modiferType].removeModifier(_modifier);
+        }
+    }
+    
+    public void addBuff(Buff _buff){
+        
+        foreach(Modifier mod in _buff.modifiers)
+        {
+            if(stats.ContainsKey(mod.modiferType))
+            {
+                stats[mod.modiferType].addModifer(mod);
+            }
+        }
+        buffs.Add(_buff, _buff.buffDuration);
+    }
+
+    private void removeBuff(Buff _buff){
+        
+        foreach(Modifier mod in _buff.modifiers)
+        {
+            if(stats.ContainsKey(mod.modiferType))
+            {
+                stats[mod.modiferType].removeModifier(mod);
+            }
+        }
+    }
+
+    public void takeDamage(float baseAmount, ModiferType damageType) 
+    {
+        stats[ModiferType.CURR_HEALTH].modifiedValue -= (baseAmount * (1 - stats[damageType].modifiedValue)); // FORMULA FOR DAMAGE RESISTANCE;
+    }
+
+
+    private void Update() {
+        processBuffs();
+    }
+
+    private void processBuffs()
+    {
+        foreach(KeyValuePair<Buff, float> buff in buffs){ 
+            buffs[buff.Key] = buff.Value - Time.deltaTime;
+            if(buffs[buff.Key] <= 0)
+            {
+                removeBuff(buff.Key);
+                buffs.Remove(buff.Key);
+            }
+        }
+    }
+    
+}
