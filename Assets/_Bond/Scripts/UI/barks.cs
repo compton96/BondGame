8ab06f9e3395abcc6338b8ee40088de4142d388d
Jacob Compton <jrcompto@ucsc.edu>
2021-02-28 15:07:20 -0800

    Added simple shopkeeper barks

---
+++ Assets/_Bond/Scripts/UI/barks.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class barks : MonoBehaviour
+{
+    public List<string> barkList = new List<string>();
+    public TextMeshProUGUI barkBox;
+    
+    private bool doBarks;
+    private float timer = 0;
+    private float showTimer = 10;
+    private float textFadeTimer = 5;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.transform.tag == "Player")
+        {
+            // Debug.Log("Do Barks");
+            doBarks = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.transform.tag == "Player")
+        {
+            // Debug.Log("Dont Barks");
+            doBarks = false;
+        }
+    }
+    
+    private void Update() {
+
+        timer+= Time.deltaTime;
+        if(doBarks)
+        {
+            barkBox.enabled = true;
+            if(timer >= textFadeTimer)
+            {
+               barkBox.text = "";
+            }
+            if(timer >= showTimer)
+            {
+                barkBox.text = barkList[Random.Range(0,barkList.Count)];
+                timer = 0;
+            }
+        } 
+        else
+        {
+            barkBox.enabled = false;
+        } 
+    }
+}
